dbfab08443be9fd08f6f94c37c5629dce4010476
raustime <raustime@gmail.com>
2020-07-21 14:46:51 +0300

    Routing first step finished

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -3,8 +3,7 @@ import {Emitter} from '@core/Emitter';
 import {StoreSubscriber} from '@core/StoreSubscriber';
 
 export class Excel {
-  constructor(selector, options) {
-    this.$el = $(selector);
+  constructor(options) {
     this.components = options.components || [];
     this.store= options.store;
     this.emitter=new Emitter();
@@ -20,10 +19,6 @@ export class Excel {
     this.components=this.components.map(Component => {
       const $el = $.create('div', Component.className);
       const component = new Component($el, componentOptions);
-      // DEBUG
-      // if (component.name) {
-      //   window['c'+component.name]=component;
-      // }
       $el.html(component.toHTML());
       $root.append($el);
       return component;
@@ -32,8 +27,7 @@ export class Excel {
     return $root;
   }
 
-  render() {
-    this.$el.append(this.getRoot());
+  init() {
     this.subscriber.subscribeComponents(this.components);
     this.components.forEach(component => component.init());
   }
